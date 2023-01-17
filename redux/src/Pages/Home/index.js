8a62dd493934ca4ada112a441b463e5252796ba7
gabriel dantas de oliveira <gabriel36dantas@gmail.com>
2023-01-17 18:27:23 -0300

    Learning Redux

--- redux/src/Pages/Home/index.js
+++ redux/src/Pages/Home/index.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import api from "../../Services/api";
 
 export default () => {
+
+    const [stock , setStock] = useState([]);
+    const [trips , setTrips] = useState([]);
+
+    useEffect(() => {
+        async function loadInfo() {
+            const response = await api.get('');
+            setStock(JSON.stringify(response.data["stock"]));
+            setTrips(JSON.stringify(response.data["trips"]));
+            response.data["trips"].map((r) => {
+                console.log(r);
+            })
+        }
+
+        loadInfo();
+    })
+
     return(
         <div>
-            home
+            <div className="box">
+                {trips}
+            </div>
         </div>
     )
 }
